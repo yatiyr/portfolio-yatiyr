c1d5c87b17be1ff32ee02290cc4d92b47b5ccad1
yatiyr <dereeren@yahoo.com>
2021-08-28 19:56:28 +0300

    playing around with mongoDB

--- portfolio-yatiyr-api/index.js
+++ portfolio-yatiyr-api/index.js
@@ -1,14 +1,18 @@
 
 
 const express = require('express');
-const server = express();
+const server = express(); 
 
-server.get('/test', (req, res) => {
-    return res.json({message: 'test is working!'});
-});
+async function runServer() {
+    await require('./db').connect();
 
-const PORT = parseInt(process.env.PORT, 10) || 3001;
-server.listen(PORT, (err) => {
-    if(err) console.error(err);
-    console.log('Server ready on port:', PORT);
-})
+    server.use('/api/v1/portfolios', require('./routes/portfolios'));
+
+    const PORT = parseInt(process.env.PORT, 10) || 3001;
+    server.listen(PORT, (err) => {
+        if(err) console.error(err);
+        console.log('Server ready on port:', PORT);
+    })    
+}
+
+runServer();
