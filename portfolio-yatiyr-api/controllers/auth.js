0aace8cc1074e98cf16d08c4fb4d11dc4383eab7
yatiyr <dereeren@yahoo.com>
2021-08-31 18:35:03 +0300

    role checking with middleware

--- portfolio-yatiyr-api/controllers/auth.js
+++ portfolio-yatiyr-api/controllers/auth.js
@@ -1,6 +1,7 @@
 
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const config = require('../config/dev');
 
 // Authentication    middleware  this  middleware  will check
 // access token  in  authorization   headers   of  a  request
@@ -16,3 +17,14 @@ exports.checkJwt = jwt({
     issuer: 'https://dev-itp-s7l7.us.auth0.com/',
     algorithms: ['RS256']
 });
+
+
+exports.checkRole = (role) => (req, res, next) => {
+    const user = req.user;
+    if(user && user[config.AUTH0_NAMESPACE + '/roles'].includes(role)) {
+        next();
+    }
+    else {
+        return res.status(401).send('You are not authorized to access this resource!');
+    }
+}
